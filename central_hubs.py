d9d47e9b1e5721e922019a9626ac8c338169b831
Suhyab Shaik <shaik.suhyab3@gmail.com>
2026-04-23 14:06:34 -0500

    Added functionality to get all active players

--- central_hubs.py
+++ central_hubs.py
@@ -5,3 +5,14 @@ Central Hub
     list<Node> return_new_neighbors(list<Node> existing_neighbors):
         // return list of existing + new neighbors
 """
+
+class central_hub:
+    def __init__(self):
+        self.nodes = []
+
+    def get_active_players(self, current_player):
+        new_players = []
+        for node in self.nodes:
+            if node != current_player and node.active:
+                new_players.append(node)
+        return new_players
